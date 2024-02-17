4fd41c1ee68cb967afa921114a1e917580f36b75
Anton Filippovich <antonfilka@tut.by>
2024-02-17 23:46:07 +0300

    feat: implement map component

---
+++ src/components/Map/Map.tsx
@@ -0,0 +1,210 @@
+"use client";
+import { MapContainer, useMap, GeoJSON } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import { useEffect } from "react";
+import BelarusGeoJSON from "./BelarusBorder.json";
+import MinskRegion from "./Regions/MinskRegion.json";
+import BrestRegion from "./Regions/BrestRegion.json";
+import MogilevRegion from "./Regions/MogilevRegion.json";
+import VitebskRegion from "./Regions/VitebskRegion.json";
+import GomelRegion from "./Regions/GomelRegion.json";
+import GrodnoRegion from "./Regions/GrodnoRegion.json";
+import L from "leaflet";
+import { FeatureCollection } from "geojson";
+import { gsap } from "gsap";
+import { renderToString } from "react-dom/server";
+
+// const CustomMarkerComponent = ({ text }: { text: string }) => {
+//   return <section></section>;
+// };
+
+function SetViewToGeoJSON({
+  data,
+  city,
+}: {
+  data: FeatureCollection;
+  city: string;
+}) {
+  const map = useMap();
+
+  useEffect(() => {
+    const geoJSONLayer = L.geoJSON(data);
+    map.fitBounds(geoJSONLayer.getBounds(), { padding: [400, 400] });
+    map.setMaxBounds(geoJSONLayer.getBounds().pad(1));
+    map.setMinZoom(7);
+    map.setMaxZoom(8);
+
+    // const customMarkerHtml = renderToString(
+    //   <CustomMarkerComponent text="Hello, World!" />
+    // );
+
+    // const customIcon = L.divIcon({
+    //   className: "my-custom-icon", // Custom class for styling
+    //   html: customMarkerHtml,
+    // });
+
+    // L.marker([52.0976214, 23.7340503], { icon: customIcon }).addTo(map);
+    // L.marker([55.1848061, 30.201622], { icon: customIcon }).addTo(map);
+    // L.marker([54.2825201, 30.990449], { icon: customIcon }).addTo(map);
+  }, [data, map]);
+
+  useEffect(() => {
+    let focusData;
+
+    if (city === "Minsk") {
+      focusData = L.geoJSON(MinskRegion as FeatureCollection);
+    } else if (city === "Brest") {
+      focusData = L.geoJSON(BrestRegion as FeatureCollection);
+    } else if (city === "Vitebsk") {
+      focusData = L.geoJSON(VitebskRegion as FeatureCollection);
+    } else if (city === "Mogilev") {
+      focusData = L.geoJSON(MogilevRegion as FeatureCollection);
+    } else if (city === "Grodno") {
+      focusData = L.geoJSON(GrodnoRegion as FeatureCollection);
+    } else if (city === "Gomel") {
+      focusData = L.geoJSON(GomelRegion as FeatureCollection);
+    } else if (city === "Country") {
+      focusData = L.geoJSON(data as FeatureCollection);
+      map.flyToBounds(focusData.getBounds().pad(0.6), { duration: 1 });
+    }
+    if (focusData && city !== "Country") {
+      map.flyToBounds(focusData.getBounds().pad(0.35), { duration: 1 });
+    }
+  }, [city, map, data]);
+
+  return null;
+}
+
+function AnimatedGeoJSON({ data }: { data: FeatureCollection }) {
+  const map = useMap();
+
+  useEffect(() => {
+    const onEachFeature = (
+      feature: any,
+      layer: {
+        on: (arg0: {
+          mouseover: (e: any) => void;
+          mouseout: (e: any) => void;
+        }) => void;
+      }
+    ) => {
+      layer.on({
+        mouseover: (e) => {
+          const layer = e.target;
+          layer.setStyle({
+            width: 2,
+            fillOpacity: 0.6, // You can change the opacity on hover if desired
+            opacity: 0.8, // You can change the opacity on hover if desired
+          });
+        },
+        mouseout: (e) => {
+          geoJsonLayer.resetStyle(e.target); // Reset to initial style on mouse out
+        },
+      });
+    };
+
+    // Assuming `data` is your GeoJSON object
+    const geoJsonLayer = L.geoJSON(data, {
+      style: () => ({
+        fillColor: "#ee964b",
+        fillOpacity: 0.7,
+        color: "#0d3b66", // Initial color
+        weight: 2, // Initial weight of the line
+        opacity: 1, // Initial opacity
+      }),
+      onEachFeature: onEachFeature,
+    }).addTo(map);
+
+    // Function to animate the GeoJSON layer
+    function animateGeoJSONLayer(layer: any) {
+      let tl = gsap.timeline({
+        repeat: 3,
+        defaults: { ease: "power2", duration: 3 },
+      });
+
+      tl.to(layer, {
+        weight: 2,
+        opacity: "0.6",
+        onUpdate: () => updateLayerStyle(layer),
+      }).to(layer, {
+        weight: 2,
+        opacity: "0.7",
+        onUpdate: () => updateLayerStyle(layer),
+      });
+
+      return tl;
+    }
+
+    // Function to apply updated styles to the GeoJSON layer
+    function updateLayerStyle(layer: any) {
+      layer.setStyle({
+        weight: gsap.getProperty(layer, "weight"),
+        opacity: gsap.getProperty(layer, "opacity"),
+      });
+    }
+
+    // Start the animation
+    const tl = gsap.timeline();
+    tl.add(animateGeoJSONLayer(geoJsonLayer));
+    tl.play();
+
+    // Cleanup on component unmount
+    return () => {
+      map.removeLayer(geoJsonLayer);
+    };
+  }, [data, map]); // Dependency array
+
+  return null;
+}
+
+const Map = ({ city }: { city: string }) => {
+  useEffect(() => {
+    const elements = document.querySelectorAll(".leaflet-bottom.leaflet-right");
+    elements.forEach(
+      (element) => element.parentNode && element.parentNode.removeChild(element)
+    );
+  }, []);
+
+  return (
+    <div className="fade-content-ver w-[82%] h-[100%] mx-auto overflow-hidden absolute left-0 top-0 my-auto">
+      <div className="fade-content-hor w-[100%] h-[100%] overflow-hidden ">
+        <MapContainer
+          className="absolute top-[50%] left-[50%] translate-x-[-52%] translate-y-[-50%]"
+          style={{
+            backgroundImage:
+              "url('http://www.asia-turismo.com/imagens/asia-imagem.jpg')",
+            width: "2400px",
+            height: "2010px",
+            margin: "0px",
+          }}
+          scrollWheelZoom={true}
+          zoomControl={false}
+          zoomSnap={0.1}
+          wheelDebounceTime={60}
+        >
+          {/* <GeoJSON
+            data={BelarusGeoJSON as FeatureCollection}
+            style={{
+              fillColor: "transparent",
+              color: "#42000c5e",
+              weight: 2,
+              opacity: 1,
+            }}
+          /> */}
+          <AnimatedGeoJSON data={MinskRegion as FeatureCollection} />
+          <AnimatedGeoJSON data={BrestRegion as FeatureCollection} />
+          <AnimatedGeoJSON data={GrodnoRegion as FeatureCollection} />
+          <AnimatedGeoJSON data={VitebskRegion as FeatureCollection} />
+          <AnimatedGeoJSON data={MogilevRegion as FeatureCollection} />
+          <AnimatedGeoJSON data={GomelRegion as FeatureCollection} />
+          <SetViewToGeoJSON
+            city={city}
+            data={BelarusGeoJSON as FeatureCollection}
+          />
+        </MapContainer>
+      </div>
+    </div>
+  );
+};
+
+export default Map;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,13 @@
+import { nextui } from "@nextui-org/react";
 import type { Config } from "tailwindcss";
+import defaultTheme from "tailwindcss/defaultTheme";
 
 const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
@@ -15,6 +18,50 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  darkMode: "class",
+  plugins: [
+    nextui({
+      themes: {
+        main: {
+          extend: "dark", // <- inherit default values from dark theme
+          colors: {
+            background: "#faf0ca",
+            foreground: "#0d3b66",
+            primary: {
+              50: "#f4d35e",
+              100: "#520F83",
+              200: "#7318A2",
+              300: "#9823C2",
+              400: "#c031e2",
+              500: "#DD62ED",
+              600: "#F182F6",
+              700: "#FCADF9",
+              800: "#FDD5F9",
+              900: "#FEECFE",
+              DEFAULT: "#f4d35e",
+              foreground: "#000000",
+            },
+            secondary: {
+              DEFAULT: "#0d3b66",
+            },
+            focus: "#D8572A",
+          },
+          layout: {
+            disabledOpacity: "0.3",
+            radius: {
+              small: "4px",
+              medium: "6px",
+              large: "8px",
+            },
+            borderWidth: {
+              small: "1px",
+              medium: "2px",
+              large: "3px",
+            },
+          },
+        },
+      },
+    }),
+  ],
 };
 export default config;
