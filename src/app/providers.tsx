a1f3b8e4bccc4dd68d465bd13454ddef8c79fbe4
Anton Filippovich <antonfilippovich.job@gmail.com>
2024-04-12 16:33:53 +0300

    feat: add speed insights

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -3,18 +3,22 @@
 import { TooltipProvider } from "@radix-ui/react-tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { SessionProvider } from "next-auth/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Suspense } from "react";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   const queryClient = new QueryClient();
 
   return (
-    <SessionProvider>
-      <Suspense fallback={null}>
-        <QueryClientProvider client={queryClient}>
-          <TooltipProvider>{children}</TooltipProvider>
-        </QueryClientProvider>
-      </Suspense>
-    </SessionProvider>
+    <>
+      <SpeedInsights />
+      <SessionProvider>
+        <Suspense fallback={null}>
+          <QueryClientProvider client={queryClient}>
+            <TooltipProvider>{children}</TooltipProvider>
+          </QueryClientProvider>
+        </Suspense>
+      </SessionProvider>
+    </>
   );
 }
